614e0e8fbc0ee2b7234b4f11e0b71b8440f9f488
Filip <filamalek@hotmail.com>
2016-11-22 19:36:25 +0100

    Added if statements inside the script file!

--- src/script/myJs.js
+++ src/script/myJs.js
@@ -27,59 +27,70 @@ function buyStocks(){
   // ajax for buy function
     $("#buyButton").click(function(){
       var id = $("#buyField").val();
-      console.log("This is the ID:"+id);
-      $.ajax({
-        'url': apiLink,
-        'type' : 'GET',
-        'data':{
-          'what':'buy',
-          'offer': id,
-          'apikey' : apiKey
-        },
-        'success':function(dataIn){
-          console.log(dataIn);
-          var out = JSON.parse(dataIn);
-          console.log(out);
-          // all the fields retrieved from
-          var id = out.data.id;
-          console.log(id);
-          var oc = out.data.offerCurrency;
-          var bc = out.data.buyCurrency;
-          var amount = out.data.amount;
-          var ot = out.data.offerTime;
-          var bt = out.data.buyTime;
+      if(id!=0){
+        console.log("Id is not zero!");
+        console.log("This is the ID:"+id);
+        $.ajax({
+          'url': apiLink,
+          'type' : 'GET',
+          'data':{
+            'what':'buy',
+            'offer': id,
+            'apikey' : apiKey
+          },
+          'success':function(dataIn){
+            console.log(dataIn);
+            var out = JSON.parse(dataIn);
+            console.log(out);
+            // all the fields retrieved from
+            var id = out.data.id;
+            console.log(id);
+            var oc = out.data.offerCurrency;
+            var bc = out.data.buyCurrency;
+            var amount = out.data.amount;
+            var ot = out.data.offerTime;
+            var bt = out.data.buyTime;
 
-          var total = "Buy ID:"+id+" Offer:"+oc+" Buy:"+bc+" Amount:"+amount+" Offer Time:"+ot+" Buy Time:"+bt;
-          $("#buyGood").append(total);
-        }
-      });
+            var total = "Buy ID:"+id+" Offer:"+oc+" Buy:"+bc+" Amount:"+amount+" Offer Time:"+ot+" Buy Time:"+bt;
+            $("#buyGood").append(total);
+          }
+        });
+      }
+      else{
+        console.log("You need to enter a valid value");
+      }
     });
 }
 function sellStocks(){
   $("#sellButton").click(function(){
     // ajax to sell stacks
     var money = $("#sellField").val();
-    $.ajax({
-      'url': apiLink,
-      'type' : 'GET',
-      'data':{
-        'what':'sell',
-        'amount': money,
-        'apikey' : apiKey
-      },
-      'success':function(dataIn){
-        console.log(dataIn);
-        var output = JSON.parse(dataIn);
-        console.log(output);
-        var id = output.data.id;
-        var amount = output.data.amount;
-        var currency = output.data.currency;
-        var time = output.data.since;
+    if(money!=0){
+      $.ajax({
+        'url': apiLink,
+        'type' : 'GET',
+        'data':{
+          'what':'sell',
+          'amount': money,
+          'apikey' : apiKey
+        },
+        'success':function(dataIn){
+          console.log(dataIn);
+          var output = JSON.parse(dataIn);
+          console.log(output);
+          var id = output.data.id;
+          var amount = output.data.amount;
+          var currency = output.data.currency;
+          var time = output.data.since;
 
-        var total = "Offer ID: "+id+" Amount:"+amount+" Currency:"+currency+" Time:"+time;
-        $("#sellGood").append(total);
-      }
-    });
+          var total = "Offer ID: "+id+" Amount:"+amount+" Currency:"+currency+" Time:"+time;
+          $("#sellGood").append(total);
+        }
+      });
+    }
+    else{
+      console.log("Enter a valid value!");
+    }
   });
 }
 function seeOffers(){
@@ -136,23 +147,28 @@ function seeExchangeRate(){
   $("#checkButton").click(function(){
     var from = $("#fromInput").val();
     var to = $("#toInput").val();
-    $.ajax({
-      'url': apiLink,
-      'type' : 'GET',
-      'data':{
-        'what':'exchange_rate',
-        'apikey' : apiKey,
-        'from': from,
-        'to':to
-      },
-      'success':function(dataIn){
-        var out = JSON.parse(dataIn);
-        console.log(out);
-        var from = out.data.from;
-        var to = out.data.to;
-        var amount = out.data.amount;
-        $("#exchange_rateParagraph").append("From:"+from+" To:"+to+" Amount: "+amount);
-      }
-    });
+    if(from!=0 && to!=0){
+      $.ajax({
+        'url': apiLink,
+        'type' : 'GET',
+        'data':{
+          'what':'exchange_rate',
+          'apikey' : apiKey,
+          'from': from,
+          'to':to
+        },
+        'success':function(dataIn){
+          var out = JSON.parse(dataIn);
+          console.log(out);
+          var from = out.data.from;
+          var to = out.data.to;
+          var amount = out.data.amount;
+          $("#exchange_rateParagraph").append("From:"+from+" To:"+to+" Amount: "+amount);
+        }
+      });
+    }
+    else {
+      console.log("Enter a valid value!");
+    }
   });
 }
